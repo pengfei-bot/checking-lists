cf78c9a6b5993c9835771b6806e429a098cfb8e8
Développeur Pop <pop@famlist.local>
2026-09-19 18:19:29 +0000

    chore(build): BuildStamp a5638fb

--- src/components/BuildStamp.tsx
+++ src/components/BuildStamp.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 import { rewardsUi } from "../theme/rewardsUi";
 
 /** Visible QA marker so smoke tests know which Pages bundle is live. */
-export const BUILD_STAMP = "7fef30c";
+export const BUILD_STAMP = "a5638fb";
 
 export function BuildStamp() {
   return (
