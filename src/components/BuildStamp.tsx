56ec5683668c9d7a933626e06c44c14dd12b3c19
Développeur Pop <pop@famlist.local>
2026-09-19 20:06:24 +0000

    chore(build): mark join-approval build

--- src/components/BuildStamp.tsx
+++ src/components/BuildStamp.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 import { rewardsUi } from "../theme/rewardsUi";
 
 /** Visible QA marker so smoke tests know which Pages bundle is live. */
-export const BUILD_STAMP = "a901a25";
+export const BUILD_STAMP = "0359486";
 
 export function BuildStamp() {
   return (
