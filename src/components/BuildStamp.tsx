450d2aca0020beba68473e993f1938dc9eab5be5
Développeur Pop <pop@famlist.local>
2026-09-19 17:23:46 +0000

    chore(build): BuildStamp 7d45f77

--- src/components/BuildStamp.tsx
+++ src/components/BuildStamp.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 import { rewardsUi } from "../theme/rewardsUi";
 
 /** Visible QA marker so smoke tests know which Pages bundle is live. */
-export const BUILD_STAMP = "0ef2cc9";
+export const BUILD_STAMP = "7d45f77";
 
 export function BuildStamp() {
   return (
