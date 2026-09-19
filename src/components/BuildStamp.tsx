bad4a0041e9040d0276eb342c4f51955078c8e09
Développeur Pop <pop@famlist.local>
2026-09-19 20:28:36 +0000

    chore(build): BuildStamp 2d5da8e

--- src/components/BuildStamp.tsx
+++ src/components/BuildStamp.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 import { rewardsUi } from "../theme/rewardsUi";
 
 /** Visible QA marker so smoke tests know which Pages bundle is live. */
-export const BUILD_STAMP = "0359486";
+export const BUILD_STAMP = "2d5da8e";
 
 export function BuildStamp() {
   return (
